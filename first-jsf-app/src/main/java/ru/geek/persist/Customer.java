9278dfabe8a5a9c146d4ae6ad034757fbc21d059
ParadiseRaider <ege_jc_god@mail.ru>
2021-04-08 23:59:36 +0300

    Сделано задание к уроку 5

--- first-jsf-app/src/main/java/ru/geek/persist/Customer.java
+++ first-jsf-app/src/main/java/ru/geek/persist/Customer.java
@@ -4,18 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "customers")
+@NamedQueries({
+        @NamedQuery(name = "deleteCustomerById", query = "delete from Customer c where c.id = :id"),
+        @NamedQuery(name = "findAllCustomer", query = "from Customer c"),
+        @NamedQuery(name = "countCustomer", query = "select count(c) from Customer c")
+})
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     @NotNull
     private String name;
 
+    @Column
     @Positive
     private Integer age;
 }
